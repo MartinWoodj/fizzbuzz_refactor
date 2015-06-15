32a6ef0100a20c8512a3b918a062188ac7808689
Joseph Wood <josephwood@Josephs-MacBook-Pro.local>
2015-06-15 09:42:08 -0400

    Needed to add jQuery script to work

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ function fizzBuzz (number) {
   for (var i = 0; i <= number; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
+      $('#numberList').append('<li>'+"FizzBuzz"+'</li>');
+    } 
+    else if (i % 3 === 0) {
       console.log("Fizz");
-    } else if (i % 5 === 0) {
+      $('#numberList').append('<li>'+"Fizz"+'</li>');
+    } 
+    else if (i % 5 === 0) {
       console.log("Buzz");
-    } else {
+      $('#numberList').append('<li>'+"Buzz"+'</li>');
+    } 
+    else {
       console.log(i);
+      $('#numberList').append('<li>'+i+'</li>');
     }
   }
 }
@@ -16,7 +23,8 @@ function getNumber() {
   var number = prompt("Enter a number.");
   
   number = parseInt(number);
-  /*console.log(typeof(number));*/
+  console.log(typeof(number));
   
   fizzBuzz(number);
 }
+
